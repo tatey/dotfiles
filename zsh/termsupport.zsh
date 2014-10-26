e41455555fa5b4af78d768caab214e0d8b43b8ce
Tate Johnson <tate@tatey.com>
2014-10-26 12:40:02 +1000

    Delete more cruft.

--- zsh/termsupport.zsh
+++ zsh/termsupport.zsh
@@ -14,20 +14,3 @@ function title {
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
 ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
-
-#Appears when you have the prompt
-function omz_termsupport_precmd {
-  title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
-}
-
-#Appears at the beginning of (and during) of command execution
-function omz_termsupport_preexec {
-  emulate -L zsh
-  setopt extended_glob
-  local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
-  title "$CMD" "%100>...>$2%<<"
-}
-
-autoload -U add-zsh-hook
-add-zsh-hook precmd  omz_termsupport_precmd
-add-zsh-hook preexec omz_termsupport_preexec
