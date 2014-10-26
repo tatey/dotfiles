2b7e148a2f57b27a3ec64452421773ce5a804c66
Tate Johnson <tate@tatey.com>
2014-10-26 22:15:24 +1000

    Simpler git prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -10,10 +10,10 @@ setopt cdablevarS
 PROMPT='%{$fg[white]%}%c$(git_prompt_info)$ % %{$reset_color%}'
 
 # git theming default: Variables for theming the git info prompt
-ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
-ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
-ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
+ZSH_THEME_GIT_PROMPT_PREFIX="("  # Prefix at the very beginning of the prompt, before the branch name
+ZSH_THEME_GIT_PROMPT_SUFFIX=""   # At the very end of the prompt
+ZSH_THEME_GIT_PROMPT_DIRTY="*)"  # Text to display if the branch is dirty
+ZSH_THEME_GIT_PROMPT_CLEAN=")"   # Text to display if the branch is clean
 
 # Setup the prompt with pretty colors
 setopt prompt_subst
