92f38cc28e858b69c3fc48ed9fe0bb1de24b56cd
Tate Johnson <tate@tatey.com>
2014-11-02 23:04:22 +1000

    Cleanup.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,7 +6,7 @@ ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G
 setopt multios
 setopt cdablevarS
 
-# Set title
+# Set tab title
 precmd () {
   print -Pn "\e]2;%c\a"
 }
