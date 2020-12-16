d32ebabce4d5c5ee540abfe69607aa454e24303c
Tate Johnson <tate@tatey.com>
2020-12-16 15:04:50 +1000

    Source nix daemon
    Works around macOS making /etc/zshrc pristine during an update.

---
+++ zsh/nix.zsh
@@ -0,0 +1,6 @@
+# See https://github.com/NixOS/nix/issues/3616
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
