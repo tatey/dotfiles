94e23526560a249465cba1ea332c58b76d6e9de6
Tate Johnson <tate@tatey.com>
2023-05-15 21:33:22 +1000

    Update Homebrew config.

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -1,2 +1,8 @@
 # Stop Homebrew from automatically updating. I guess this is years of me doing apt-get update; apt-get upgrade
 export HOMEBREW_NO_AUTO_UPDATE=1
+
+# Stop Homebrew from sending analytics
+export HOMEBREW_NO_ANALYTICS=1
+
+# Stop Homebrew from giving hints
+export HOMEBREW_NO_ENV_HINTS=1
